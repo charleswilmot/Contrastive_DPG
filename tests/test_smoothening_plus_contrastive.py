dab111cfe229f4f027daf552e5a46f36b63bcfe3
Charles <charles.wilmot2@hotmail.fr>
2022-02-06 20:07:15 +0100

    experimentation with smoothing loss

--- tests/test_smoothening_plus_contrastive.py
+++ tests/test_smoothening_plus_contrastive.py
@@ -2,16 +2,32 @@ import jax
 import jax.numpy as jnp
 from typing import Callable
 from scipy.special import binom
+from typing import Callable
 
 
-def get_derivatives(a, n):
-    ret = [a]
-    for i in range(n):
-        ret.append(ret[-1][1:] - ret[-1][:-1])
-        # ret.append((ret[-1][1:] - ret[-1][:-1]) / (i + 1))
-    return ret[1:]
+def squared_distance(x1: jnp.ndarray, x2: jnp.ndarray) -> jnp.ndarray:
+    return jnp.sum((x1 - x2) ** 2, axis=-1)
 
 
+def euclidian_distance(x1: jnp.ndarray, x2: jnp.ndarray) -> jnp.ndarray:
+    return jnp.sqrt(1e-6 + jnp.sum((x1 - x2) ** 2, axis=-1))
+
+
+def distance_matrix(func: Callable, x: jnp.ndarray) -> jnp.ndarray:
+    return jax.vmap(
+        lambda x1: jax.vmap(
+            lambda x2:
+                func(x1, x2)
+        )(x)
+    )(x) # shape [D0, D0, D1, ..., DN-1]
+
+
+def contrastive(matrix, n, r_ilambda, r_height, a_ilambda, a_height, d_max):
+    matrix = jnp.sort(matrix, axis=0)[1:n+1] # [n, N, ...]
+    return (
+        2 * r_height * jax.nn.sigmoid(-matrix / r_height / r_ilambda) +
+        1 * a_height * jax.nn.sigmoid((matrix - d_max) / a_height / a_ilambda)
+    )
 
 
 def get_derivative(a, n):
@@ -32,7 +48,8 @@ def fun1():
     plt.ion()
 
     n_points = 100
-    smoothening_coef = 0.5
+    smoothening_coef = 1.1
+    contrastive_coef = 0.1
     learning_rate = 1e-2
 
 
@@ -50,11 +67,26 @@ def fun1():
         # nothing
         distance_loss = 0
         epsilon = 1e-5
-        # smoothening_loss = sum(jnp.sum(jnp.sqrt(epsilon + jnp.sum(x ** 2, axis=-1))) for x in get_derivatives(positions, n=2))
         derivative = get_derivative(positions, n=2)
-        # print(derivative.shape)
         smoothening_loss = jnp.sum(jnp.sqrt(jnp.sum(derivative ** 2, axis=-1)))
-        return distance_loss + smoothening_coef * smoothening_loss
+
+
+
+        contrastive_loss = jnp.sum(contrastive(
+            distance_matrix(euclidian_distance, positions),
+            n=20,
+            r_ilambda=0.03,
+            r_height=2,
+            a_ilambda=0.7,
+            a_height=0.7,
+            d_max=3,
+        ))
+
+
+        return distance_loss + smoothening_coef * smoothening_loss + contrastive_coef * contrastive_loss
+
+
+
 
     dloss_dpositions = jax.grad(loss)
 
