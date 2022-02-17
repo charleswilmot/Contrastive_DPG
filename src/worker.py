0979ba621fcf1cb4b32cfd3351b466dcf7c87596
Charles <charles.wilmot2@hotmail.fr>
2022-02-17 14:11:22 +0100

    worker configurable from command line

--- src/worker.py
+++ src/worker.py
@@ -8,6 +8,7 @@ import re
 import datetime
 from experiment import Experiment
 from agent import Agent
+import argparse
 
 
 if __name__ == '__main__':
@@ -18,15 +19,23 @@ if __name__ == '__main__':
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
+    parser = argparse.ArgumentParser(description='Executes jobs from the DB until non remain.')
+    parser.add_argument('--host', default='127.0.0.1', help='MySQL DB IP address')
+    parser.add_argument('--user', default='root', help='username for MySQL DB')
+    parser.add_argument('--password', default='', help='password for MySQL DB')
+    parser.add_argument('--db-name', default='Contrastive_DQN_debug', help='name for MySQL DB')
+    parser.add_argument('--log-path', default='../experiments/', help='path where experiments are logged')
 
-    db = Database(sys.argv[1])
+    args = parser.parse_args()
+
+    db = Database(db_name=args.db_name, user=args.user, password=args.password, host=args.host)
 
 
     done = False
     while not done:
 
 
-        log_path = sys.argv[2]
+        log_path = args.log_path
         ids = [int(match.group(1)) for x in os.listdir(log_path) if (match := re.match('([0-9]+)_[a-zA-Z]+[0-9]+_[0-9]+-[0-9]+', x))]
         if ids:
             exp_id = 1 + max(ids)
@@ -34,6 +43,7 @@ if __name__ == '__main__':
             exp_id = 0
         run_name = f'{exp_id:03d}_{datetime.datetime.now():%b%d_%H-%M}'
         path = f'{log_path}/{run_name}'
+        os.makedirs(log_path, exist_ok=True)
         os.makedirs(path, exist_ok=True)
 
         file_handler = logging.FileHandler(f"{path}/output.log")
