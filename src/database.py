c3c9123ab65c89a6643d36f6ded5d7b701e79afe
Charles <charles.wilmot2@hotmail.fr>
2022-02-18 17:54:34 +0100

    experiment_done counter was not updating. It is fixed

--- src/database.py
+++ src/database.py
@@ -601,6 +601,7 @@ class Database:
             command = f'''UPDATE experiment_configs
                           SET
                             repetitions_running = repetitions_running - 1
+                            repetitions_done = repetitions_done + 1
                           WHERE
                             experiment_config_id={experiment_config_id}
                           '''
