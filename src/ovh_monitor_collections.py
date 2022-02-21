f9b588b17f34962e0d60907e8453bad34a2e168f
Charles <charles.wilmot2@hotmail.fr>
2022-02-21 22:46:08 +0100

    added plotting functions

--- src/ovh_monitor_collections.py
+++ src/ovh_monitor_collections.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     import sys
     import pandas as pd
     from collections import OrderedDict
+    import matplotlib.pyplot as plt
 
     logger = logging.getLogger()
     logger.setLevel(logging.CRITICAL)
@@ -18,9 +19,12 @@ if __name__ == '__main__':
     parser.add_argument('--user', default='ubuntu', help='username for MySQL DB')
     parser.add_argument('--password', default='aqwsedcft', help='password for MySQL DB')
     parser.add_argument('--db-name', default='Contrastive_DPG_debug', help='name for MySQL DB')
+    parser.add_argument('--finished-only', action='store_true', help='show finished experiments only')
     parser.add_argument('--host', default='127.0.0.1', help='IP for MySQL DB')
 
     args = parser.parse_args()
+
+    finished_only = "AND finished=1" if args.finished_only else ""
 
     db = Database(db_name=args.db_name, user=args.user, password=args.password, host=args.host)
 
@@ -49,6 +53,10 @@ if __name__ == '__main__':
         ''')
         variation_columns = exp_config_infos.loc[:, (exp_config_infos != exp_config_infos.iloc[0]).any()]
         scores = pd.DataFrame()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
         for index, info in variation_columns.iterrows():
             returns = db.get_dataframe(f'''
                 SELECT
@@ -64,13 +72,19 @@ if __name__ == '__main__':
                         FROM
                             experiments
                         WHERE
-                            experiment_config_id={info.experiment_config_id} AND
-                            finished=1
+                            experiment_config_id={info.experiment_config_id} {finished_only}
                     )
                 GROUP BY
                     episode_nb
             ''')
             if len(returns):
+                x = returns.episode_nb.values
+                y_train = returns["AVG(training_episode_return)"].values
+                label = " - ".join([f"{param_name}={val}" for param_name, val in info[2:].iteritems()])
+                ax.plot(x, y_train, label=label)
+                ax.set_xlabel("#episodes")
+                ax.set_ylabel("episode return")
+
                 score_total = returns.drop('episode_nb', 1).mean()
                 score_begining = returns.drop('episode_nb', 1)[returns.episode_nb < 4000].mean()
                 score_end = returns.drop('episode_nb', 1)[returns.episode_nb > 12000].mean()
@@ -85,6 +99,11 @@ if __name__ == '__main__':
                 ))
                 row = info[2:].append(row)
                 scores = scores.append(row, ignore_index=True, sort=False)
+
+        ax.set_xlim([0, 16000])
+        ax.set_ylim([-0.1, 2.1])
+        ax.legend(prop={'size': 6})
+        plt.savefig(f"/tmp/{collection}.png", dpi=400)
         scores = scores.reindex(row.index, axis=1)
         print("###", collection, '###')
         print(scores)
